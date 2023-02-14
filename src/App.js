bafbd72ad6493213d907ce3cbc7ce02d54c43a70
Syed Salman <syedsalman1006@gmail.com>
2023-02-14 13:42:14 +0530

    Completed coundown app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const inputRef = useRef()
+    const [time, setTime] = useState(null);
+    const [timer, setTimer] = useState(null);
+
+    useEffect(()=>{
+        if(time === 0){
+            clearInterval(timer);
+        }
+    }, [time])
+    function handleChange(e){
+        let num = parseInt(e.value);
+        if(isNaN(num) || num < 0){
+            setTime(0);
+            return false;
+        }else{
+            setTime(num);
+            return true;
+        }
+    }
+
+    function handleKey(e){
+        if(e.key === 'Enter'){
+            if(handleChange(inputRef.current)){
+                if(timer !== null){
+                    clearInterval(timer);
+                    setTimer(null);
+                }
+                let newTimer = setInterval(()=>{
+                    setTime(oldTime => oldTime - 1)
+                }, 1000)
+                setTimer(newTimer);
+            }
+        }
+    }
+
+    return (
+        <>  
+            <header>
+                <h1>Count Down</h1>
+            </header>
+            <main className="container">
+                <input
+                    type="number"
+                    name="time"
+                    id="time"
+                    ref={inputRef}
+                    onKeyDown={handleKey}
+                />
+                <div id="current-time">
+                    {time!==null && time}
+                </div>
+            </main>
+        </>
+    )
 }
-
 export default App;
